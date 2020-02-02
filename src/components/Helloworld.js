6113de35f0e068c94e720396ede636eb8ef08bde
unknown <galaxy.net0710@gmail.com>
2020-02-02 15:36:01 -0600

    mejora al loader y al header

--- src/components/Helloworld.js
+++ src/components/Helloworld.js
@@ -3,15 +3,14 @@ import '../assets/styles/index.scss';
 import logo from '../assets/static/logo.png';
 import Pokemon from './pokemon';
 
-
 const HelloWorld = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <img src={logo} width='190' height='90' className='d-inline-block  ' alt='logo' />
-        </nav>
-      </header>
+      <div className='head'>
+
+        <img src={logo} width='210' height='110' alt='logo' />
+
+      </div>
       <div className='title'><h1>POKEDEX</h1></div>
       {/* <img src={logo} alt='logo' /> */}
       <Pokemon />
