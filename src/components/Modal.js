5747a16d34b96d00aebec7bef7d1daabbcddfddb
unknown <galaxy.net0710@gmail.com>
2020-02-25 20:33:01 -0600

    modal listo

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,27 +1,4 @@
-// import React, { useRef, useEffect } from 'react';
 
-// import { ModalBackground, ModalContainer, Close } from './styles';
-
-// const handleClickOutside = (node, closeModal) => (event) => {
-//   if (node && !node.contains(event.target)) closeModal();
-// };
-
-// export default function Modal({ open, closeModal, children }) {
-//   const ref = useRef();
-//   useEffect(() => {
-//     const handler = handleClickOutside(ref.current, closeModal);
-//     document.addEventListener('mousedown', handler);
-//     return () => document.removeEventListener('mousedown', handler);
-//   });
-//   return (
-//     //<ModalBackground  onClick={closeModal}>
-//     <ModalContainer open={open} ref={ref}>
-//       <Close onClick={closeModal} />
-//       {children}
-//     </ModalContainer>
-//     //</ModalBackground>
-//   );
-// }
 
 import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
